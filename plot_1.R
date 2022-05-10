77915612713892a5889bd6339df276f92f3875cf
Jose Molina <nicolasmolina45@hotmail.com>
2022-05-09 21:30:31 -0400

    Enviando al repositorio cuatro archivos PNG y cuatro archivos de código R, un total de ocho archivos en la carpeta de nivel superior del repositorio

---
+++ plot_1.R
@@ -0,0 +1,29 @@
+
+
+dataHousehold <- "./data/household_power_consumption.txt"
+data <- read.table(dataHousehold, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+datasetDates <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+
+#str - datasetDates
+globalActivePower <- as.numeric(datasetDates$Global_active_power)
+png("plot1.png", width=480, height=480)
+hist(globalActivePower, col="red", main="Global Active Power", xlab="Global Active Power (kilowatts)")
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ plot_4.R
@@ -0,0 +1,43 @@
+
+dataHousehold <- "./data/household_power_consumption.txt"
+data <- read.table(dataHousehold, header=TRUE, sep=";", stringsAsFactors=FALSE, dec=".")
+datasetDates <- data[data$Date %in% c("1/2/2007","2/2/2007") ,]
+
+#str - datasetDates
+datetime <- strptime(paste(datasetDates$Date, datasetDates$Time, sep=" "), "%d/%m/%Y %H:%M:%S") 
+globalActivePower <- as.numeric(datasetDates$Global_active_power)
+globalReactivePower <- as.numeric(datasetDates$Global_reactive_power)
+voltage <- as.numeric(datasetDates$Voltage)
+subMetering1 <- as.numeric(datasetDates$Sub_metering_1)
+subMetering2 <- as.numeric(datasetDates$Sub_metering_2)
+subMetering3 <- as.numeric(datasetDates$Sub_metering_3)
+
+png("plot4.png", width=480, height=480)
+par(mfrow = c(2, 2)) 
+
+plot(datetime, globalActivePower, type="l", xlab="", ylab="Global Active Power", cex=0.2)
+
+plot(datetime, voltage, type="l", xlab="datetime", ylab="Voltage")
+
+plot(datetime, subMetering1, type="l", ylab="Energy Submetering", xlab="")
+lines(datetime, subMetering2, type="l", col="red")
+lines(datetime, subMetering3, type="l", col="blue")
+legend("topright", c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), lty=, lwd=2.5, col=c("black", "red", "blue"), bty="o")
+
+plot(datetime, globalReactivePower, type="l", xlab="datetime", ylab="Global_reactive_power")
+
+dev.off()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
